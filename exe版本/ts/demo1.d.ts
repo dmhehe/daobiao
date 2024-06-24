90b9acbdf502c59cecd3606b023d44a27b33ec07
dmhehe <847990303@qq.com>
2024-06-24 21:44:41 +0800

    搞个exe版本

---
+++ exe版本/ts/demo1.d.ts
@@ -0,0 +1,12 @@
+//----&&----start----&&----
+export interface demo1Data {
+        id: number;
+    name: string;
+    zhanli: number;
+    goods: number[];
+}
+
+export interface demo1DataMap {
+    [key: string]: demo1Data;
+}
+//----&&----end----&&----
---
+++ exe版本/ts/demo2.d.ts
@@ -0,0 +1,10 @@
+//----&&----start----&&----
+export interface demo2Data {
+        id: number;
+    name: string;
+    zhanli: number;
+    goods: number[];
+}
+
+export type demo2DataArray = demo2Data[];
+//----&&----end----&&----
